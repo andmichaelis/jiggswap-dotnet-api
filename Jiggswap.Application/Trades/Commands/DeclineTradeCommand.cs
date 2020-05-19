5cdea20894370ea5213afc12df653ab2b37f64a2
andmichaelis <47260000+andmichaelis@users.noreply.github.com>
2020-05-18 19:27:47 -0500

    Update DeclineTradeCommand.cs

--- Jiggswap.Application/Trades/Commands/DeclineTradeCommand.cs
+++ Jiggswap.Application/Trades/Commands/DeclineTradeCommand.cs
@@ -1,4 +1,8 @@
-﻿using FluentValidation;
+﻿using Dapper;
+using FluentValidation;
+using Jiggswap.Application.Common;
+using Jiggswap.Application.Common.Interfaces;
+using Jiggswap.Application.Trades.Dtos;
 using MediatR;
 using System;
 using System.Collections.Generic;
@@ -15,16 +19,65 @@ namespace Jiggswap.Application.Trades.Commands
 
     public class DeclineTradeCommandValidator : AbstractValidator<DeclineTradeCommand>
     {
-        public DeclineTradeCommandValidator()
+        private readonly ICurrentUserService _currentUser;
+        private readonly IJiggswapDb _db;
+
+        public DeclineTradeCommandValidator(ICurrentUserService currentUser, IJiggswapDb db)
         {
+            _currentUser = currentUser;
+            _db = db;
+
+            RuleFor(t => t.TradeId)
+                .MustAsync(BeSentToCurrentUser)
+                .WithMessage("You are not a member of this trade or it is not longer proposed");
+        }
+
+        private async Task<bool> BeSentToCurrentUser(Guid tradeId, CancellationToken arg2)
+        {
+            using var conn = _db.GetConnection();
+
+            var tradeUser = await conn.QuerySingleOrDefaultAsync<int>(@"
+                select
+                    T.requested_user_id
+                from trades T
+                where T.public_id = @TradeId and T.status = @Proposed",
+                new
+                {
+                    TradeId = tradeId,
+                    TradeStates.Proposed
+                });
+
+            return tradeUser == _currentUser.InternalUserId;
         }
     }
 
     public class DeclineTradeCommandHandler : IRequestHandler<DeclineTradeCommand, bool>
     {
-        public Task<bool> Handle(DeclineTradeCommand request, CancellationToken cancellationToken)
+        private readonly IJiggswapDb _db;
+
+        public DeclineTradeCommandHandler(IJiggswapDb db)
         {
-            throw new NotImplementedException();
+            _db = db;
+        }
+
+        public async Task<bool> Handle(DeclineTradeCommand request, CancellationToken cancellationToken)
+        {
+            using var conn = _db.GetConnection();
+
+            await conn.ExecuteAsync(@"
+                update
+                    trades
+                set
+                    status = @Inactive
+                where
+                    public_id = @TradeId",
+                 new
+                 {
+                     TradeStates.Inactive,
+                     request.TradeId
+                 });
+
+            return true;
         }
     }
 }
